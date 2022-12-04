af46c477c8f44734313c45468c6fa30642b6f418
farzadafi <farzadafi40@gmail.com>
2022-12-04 15:11:45 +0330

    implement from PasswordHashProviderFactory and implement method

--- src/main/java/com/hashprovider/SHA256HashProviderFactory.java
+++ src/main/java/com/hashprovider/SHA256HashProviderFactory.java
@@ -1,4 +1,34 @@
 package com.hashprovider;
 
-public class SHA256HashProviderFactory {
+import org.keycloak.Config;
+import org.keycloak.credential.hash.PasswordHashProvider;
+import org.keycloak.credential.hash.PasswordHashProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class SHA256HashProviderFactory implements PasswordHashProviderFactory {
+    @Override
+    public PasswordHashProvider create(KeycloakSession keycloakSession) {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
 }
