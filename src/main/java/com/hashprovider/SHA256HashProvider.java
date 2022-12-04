fe38fb756b6eb1ad1f70140ff3a125ea7c18b0a3
farzadafi <farzadafi40@gmail.com>
2022-12-04 15:41:47 +0330

    implement encode method

--- src/main/java/com/hashprovider/SHA256HashProvider.java
+++ src/main/java/com/hashprovider/SHA256HashProvider.java
@@ -4,12 +4,15 @@ import org.keycloak.credential.hash.PasswordHashProvider;
 import org.keycloak.models.PasswordPolicy;
 import org.keycloak.models.credential.PasswordCredentialModel;
 
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.SecureRandom;
 
 public class SHA256HashProvider implements PasswordHashProvider {
 
     private final String providerId;
+    public static final String ALGORITHM = "SHA-256";
 
     public SHA256HashProvider(String providerId) {
         this.providerId = providerId;
@@ -23,7 +26,7 @@ public class SHA256HashProvider implements PasswordHashProvider {
     @Override
     public PasswordCredentialModel encodedCredential(String rawPassword, int iterations) {
         String salt = new String(generateSalt(), StandardCharsets.UTF_8);
-        String encodedPassword = this.encode( rawPassword + salt, 27500);
+        String encodedPassword = this.encode(rawPassword + salt, 27500);
         return PasswordCredentialModel.createFromValues(this.providerId, salt.getBytes(), 27500, encodedPassword);
     }
 
@@ -33,6 +36,22 @@ public class SHA256HashProvider implements PasswordHashProvider {
         String encodedPassword = this.encode(rawPassword + salt, 27500);
         String hash = passwordCredentialModel.getPasswordSecretData().getValue();
         return encodedPassword.equals(hash);
+    }
+
+    @Override
+    public String encode(String rawPassword, int iterations) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(ALGORITHM);
+            md.update(rawPassword.getBytes());
+
+            BigInteger bigInteger = new BigInteger(1, md.digest());
+
+            System.out.printf("encoded final password is %s%n", String.format("%40x", bigInteger));
+            return String.format("%040x", bigInteger);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
     @Override
