27c18697bb1b415e00ded1dda0fa77f21699f958
farzadafi <farzadafi40@gmail.com>
2022-12-04 15:14:02 +0330

    implement from PasswordHashProvider and Override needed method

--- src/main/java/com/hashprovider/SHA256HashProvider.java
+++ src/main/java/com/hashprovider/SHA256HashProvider.java
@@ -1,4 +1,29 @@
 package com.hashprovider;
 
-public class SHA256HashProvider {
+import org.keycloak.credential.hash.PasswordHashProvider;
+import org.keycloak.models.PasswordPolicy;
+import org.keycloak.models.credential.PasswordCredentialModel;
+
+public class SHA256HashProvider implements PasswordHashProvider {
+
+
+    @Override
+    public boolean policyCheck(PasswordPolicy passwordPolicy, PasswordCredentialModel passwordCredentialModel) {
+        return false;
+    }
+
+    @Override
+    public PasswordCredentialModel encodedCredential(String s, int i) {
+        return null;
+    }
+
+    @Override
+    public boolean verify(String s, PasswordCredentialModel passwordCredentialModel) {
+        return false;
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
